729e26364205f67b63a0cb35f1fd037a84e8a0ef
Raj01-create <rajendrakeshari.pal014183@rec.ac.in>
2026-06-07 22:51:47 +0530

    Implement task creation functionality
    Added functionality to create a new task item with input, edit, and delete options. in next we add the fuction

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 
+// Add a new task.
+    let taskInput = document.getElementById("new-task");
+
+    // first button
+    let addButton = document.getElementsByTagName("button")[0];
+
+    // ul of #incomplete-tasks
+    let incompleteTaskHolder = document.getElementById("incomplete-tasks");
+
+    // completed-tasks
+    let completedTasksHolder = document.getElementById("completed-tasks");
+
+    /*---- Part 1 ----*/
+    // function to create a new task item
+    let createNewTaskElement = function (taskString) {
+
+        let listItem = document.createElement("li");
+
+        // input (checkbox)
+        let checkBox = document.createElement("input");
+        // label
+        let label = document.createElement("label");
+        // input (text)
+        let editInput = document.createElement("input");
+        // button.edit
+        let editButton = document.createElement("button");
+
+        // button.delete
+        let deleteButton = document.createElement("button");
+
+        label.innerText = taskString;
+
+        // Each element needs appending
+        checkBox.type = "checkbox";
+        editInput.type = "text";
+
+
+        // innerText encodes special characters, HTML does not.
+        editButton.innerText = "Edit";
+        editButton.className = "edit";
+        deleteButton.innerText = "Delete";
+        deleteButton.className = "delete";
+
+        // Append elements
+        listItem.appendChild(checkBox);
+        listItem.appendChild(label);
+        listItem.appendChild(editInput);
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+        return listItem;
+    }
+
